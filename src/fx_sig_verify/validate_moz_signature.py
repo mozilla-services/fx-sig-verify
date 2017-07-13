bd1278fd86072acd4192ed809711d29ba0abaf61
Hal Wine <hwine@mozilla.com>
2017-07-13 15:58:14 -0700

    Always log status to CloudWatch, fixes issue #17
    Missing a print statement for the 'pass' (happy) path.

--- src/fx_sig_verify/validate_moz_signature.py
+++ src/fx_sig_verify/validate_moz_signature.py
@@ -257,9 +257,9 @@ class MozSignedObjectViaLambda(MozSignedObject):
         """
         message = self.format_message()
         if self.verbose:
-            print(message)
-            print(self.summary())
-        if valid is not None and not valid or self.verbose:
+            print("msg: '{}'".format(message))
+            print("sum: '{}'".format(self.summary()))
+        if (valid is not None and not valid) or self.verbose:
             self.send_sns(message)
 
     def summary(self):
@@ -432,4 +432,6 @@ def lambda_handler(event, context):
         artifact.report_validity()
         results.append(artifact.summary())
     response['results'] = results
+    # always output response to CloudWatch (issue #17)
+    print(response)
     return response
