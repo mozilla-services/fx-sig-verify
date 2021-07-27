dac6a87764d72d5a71fa08015520c5589ce3d198
Hal Wine <hwine@mozilla.com>
2021-07-27 15:48:33 -0700

    Add test for missing JSON in CloudWatch
    Also, black did its thing, and told bandit to not worry about the tests.

--- tests/func/test_lambda_reporting.py
+++ tests/func/test_lambda_reporting.py
@@ -1,36 +1,47 @@
 # For any case, logs should be sent to CloudWatch (aka, stdout)
 
+import re
+
 from moto import mock_s3, mock_sns, mock_sqs
 import pytest
 import tests.utils as u
 
-from fx_sig_verify.validate_moz_signature import (lambda_handler, )  # noqa: E402
+from fx_sig_verify.validate_moz_signature import (
+    lambda_handler,
+)  # noqa: E402
 
 
 @mock_s3
 @mock_sns
 @mock_sqs
-@pytest.mark.parametrize('fname', u.bad_file_names_list +
-                                  u.good_file_names_list)
-@pytest.mark.parametrize('setter', u.set_verbose_true_list[:1] +
-                                   u.set_production_false_list[:1])
-def test_always_log_output_issue_17(setter, fname, capsys):
+@pytest.mark.parametrize(
+    "fname", u.bad_file_names_list[:1] + u.good_file_names_list[:1]
+)
+@pytest.mark.parametrize("verbose", u.set_verbose_true_list + u.set_verbose_false_list)
+@pytest.mark.parametrize(
+    "production", u.set_prod_true_list[:1] + u.set_production_false_list[:1]
+)
+def test_always_log_output_issue_17(verbose, production, fname, capsys):
     # mock the queue, but we won't examine it
     u.setup_aws_mocks()
     bucket = u.create_bucket()
     # Given that VERBOSE is in any state
-    setter()
+    verbose()
+    #   and that PRODUCTION is in any state
+    production()
     # WHEN any file is processed
     bucket_name, key_name = u.upload_file(bucket, fname)
     event = u.build_event(bucket_name, key_name)
     results = lambda_handler(event, u.dummy_context)
-    # THEN there should always be a message on stdout
     out, err = capsys.readouterr()
     # put useful information in failure output
-    print("response: '{}'".format(results))
+    print(f"response: '{results}'")
     print(f"stdout:\n{out}\n\nstderr:\n{err}")
-    assert out != ''
-    assert err == ''
+    # THEN there should always be a message on stdout
+    assert out != ""
+    assert err == ""
+    #  and there should always be json on stdout
+    assert re.search("^{", out, re.MULTILINE)
 
 
 @mock_s3
@@ -43,10 +54,10 @@ def test_raise_exception_on_S3_error():
     u.create_bucket()
     # WHEN a non existant file is processed
     # (S3 only guarantees eventual consistancy)
-    bucket_name, key_name = 'bogus_bucket', 'firefox-bogus.exe'
+    bucket_name, key_name = "bogus_bucket", "firefox-bogus.exe"
     event = u.build_event(bucket_name, key_name)
     # THEN we raise an error (so AWS will retry)
     with pytest.raises(IOError):
         results = lambda_handler(event, u.dummy_context)
     # and function should not have returned
-    assert 'results' not in list(locals().keys())
+    assert "results" not in list(locals().keys())
